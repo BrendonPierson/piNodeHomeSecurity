54d0eadf130de7ff33b8ddca9c105370b191c35f
BrendonPierson <brendonpierson@gmail.com>
2015-08-01 12:33:41 -0500

    added blinking led

--- gpio.js
+++ gpio.js
@@ -1,16 +1,18 @@
 var io = (function(){
-var Gpio = require('onoff').Gpio,
-  backDoor = new Gpio(23, 'in', 'falling'),
-  pir = new Gpio(16, 'in', 'both'),
-  buzzer = new Gpio(25, 'out'),
-  button = new Gpio(12, 'in', 'both'),
-  exit = function() {
-    buzzer.unexport();
-    button.unexport();
-    backDoor.unexport();
-    pir.unexport();
-    process.exit();
-  }
+
+  var Gpio = require('onoff').Gpio,
+      backDoor = new Gpio(23, 'in', 'falling'), //blue
+      pir = new Gpio(16, 'in', 'both'), //orange
+      buzzer = new Gpio(25, 'out'), //purple
+      button = new Gpio(12, 'in', 'both'), //white
+      led = new Gpio(17, 'out'), //black
+      exit = function() {
+        buzzer.unexport();
+        button.unexport();
+        backDoor.unexport();
+        pir.unexport();
+        process.exit();
+    }
 
   return {
     soundAlarm: function(){
@@ -18,6 +20,16 @@ var Gpio = require('onoff').Gpio,
     },
     stopAlarm: function(){
       buzzer.write(0);
+    },
+    beepBeep: function(){
+      var iv = setInterval(function(){
+        led.writeSync(led.readSync() === 0 ? 1 : 0)
+      }, 500);
+      setTimeout(function() {
+        clearInterval(iv); // Stop blinking
+        led.writeSync(0);  // Turn LED off.
+        led.unexport();    // Unexport GPIO and free resources
+      }, 1000);
     },
     armedNoDelay: function(){
       backDoor.watch(function(err, value){

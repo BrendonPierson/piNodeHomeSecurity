47562a8cbae2c06d9dcdb25672f3179b223d6cf6
Brendon <brendonpierson@gmail.com>
2015-08-02 15:26:51 +0000

    basic gpio door watch and buzz function

---
+++ soundNotifier.js
@@ -0,0 +1,14 @@
+//This code is from @schurig https://github.com/schurig
+
+var platform = require('os').platform,
+               exec = require('child_process').exec;
+
+var playSound = function playSound(soundFile) {
+  if(platform() == 'linux') {
+    exec("omxplayer " + soundFile, function (error) {
+      if (error !== null) { console.log('exec error: ' + error); }
+    });
+  } else { console.log('not on linux'); }
+};
+
+module.exports = playSound;

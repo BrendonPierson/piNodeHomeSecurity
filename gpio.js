f0c82b71f98622c76ffd74f2ae6f16e6aab1acbd
BrendonPierson <brendonpierson@gmail.com>
2015-08-01 21:16:10 -0500

    got rid of return object GPIO

--- gpio.js
+++ gpio.js
@@ -1,50 +1,60 @@
-var GPIO = function(){
-
-  var Gpio = require('onoff').Gpio,
-      backDoor = new Gpio(23, 'in', 'both'), //blue
-      pir = new Gpio(16, 'in', 'both'), //orange
-      buzzer = new Gpio(25, 'out'), //purple
-      button = new Gpio(12, 'in', 'both'), //white
-      led = new Gpio(17, 'out'); //black
+var Gpio = require('onoff').Gpio,
+    backDoor = new Gpio(23, 'in', 'both'), //blue
+    pir = new Gpio(16, 'in', 'both'), //orange
+    buzzer = new Gpio(25, 'out'), //purple
+    button = new Gpio(12, 'in', 'both'), //white
+    led = new Gpio(17, 'out'); //black
       
-  return {
-    soundAlarm: function(){
-      buzzer.write(1);
-      console.log("buzz On")
-    },
-    stopAlarm: function(){
-      buzzer.write(0);
-      console.log("buzz off")
-    },
-    armedNoDelay: function(){
-	    console.log("armedNoDelay function has fired");
-      backDoor.watch(function(err, value){
-      console.log("inside backDoor.watch function");
-      if (err) exit ();
-        soundAlarm();
-      });
-    },
-    armedWithDelay: function() {
-      backDoor.watch(function(err, value){
-      if (err) exit ();
-      setTimeout(soundAlarm(), 10000);
-      });
-    },
-    exit: function() {
-      buzzer.unexport();
-      button.unexport();
-      backDoor.unexport();
-      pir.unexport();
-      led.unexport();
-      process.exit();
-    },
-  }
+/////////////////////  
+// FUNCTIONS
+/////////////////////
+
+function soundAlarm(){
+  buzzer.write(1);
+  console.log("buzz On")
 };
+
+function stopAlarm(){
+  buzzer.write(0);
+  console.log("buzz off")
+};
+
+function armedNoDelay(){
+  console.log("armedNoDelay function has fired");
+  backDoor.watch(function(err, value){
+  console.log("inside backDoor.watch function");
+  if (err) exit ();
+    soundAlarm();
+  });
+};
+
+function armedWithDelay() {
+  backDoor.watch(function(err, value){
+  if (err) exit ();
+  setTimeout(soundAlarm(), 10000);
+  });
+};
+
+function exit() {
+  buzzer.unexport();
+  button.unexport();
+  backDoor.unexport();
+  pir.unexport();
+  led.unexport();
+  process.exit();
+}
+
+
+
+
+
+
+
+
+process.on('SIGINT', exit());
+
 
 
 
  
 
-
-
-module.exports = GPIO();

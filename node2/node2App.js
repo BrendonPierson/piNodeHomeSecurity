0411f4786a97bf0442934198d4e73f50d5e34048
BrendonPierson <brendonpierson@gmail.com>
2015-09-30 09:36:50 -0500

    limited motion detection

--- node2/node2App.js
+++ node2/node2App.js
@@ -4,18 +4,23 @@ var Gpio = require('onoff').Gpio,
     Firebase = require("firebase"),
     ref = new Firebase("https://securepenning.firebaseio.com/");
 
-
-motion.watch(function(err, value){
-  if(err) exit();
-    if(value === 1) {
-      console.log("Motion detected at: ", new Date().toLocaleTimeString());
-      var options = { timeZone: 'UTC', timeZoneName: 'short' };
-      var time = new Date().toLocaleTimeString('en-US', options);
-      ref.child('motion').set('Motion Detected at: '+ time );
-      ref.child('motionVal').set(1);
-    } else {
-      ref.child('motionVal').set(0);
-    }
+ref.child('armedWithMotion').on('value', function(snapshot){
+  data = snapshot.val();
+  console.log("armed with motion fb", data);
+  if(data === true) {
+    motion.watch(function(err, value){
+      if(err) exit();
+        if(value === 1) {
+          console.log("Motion detected at: ", new Date().toLocaleTimeString());
+          var options = { timeZone: 'UTC', timeZoneName: 'short' };
+          var time = new Date().toLocaleTimeString('en-US', options);
+          ref.child('motion').set('Motion Detected at: '+ time );
+          ref.child('motionVal').set(1);
+        } else {
+          ref.child('motionVal').set(0);
+        }
+    });
+  }
 });
 
 

7a52a6511e8a60dc05150834937ea85adaeef172
BrendonPierson <brendonpierson@gmail.com>
2015-10-02 10:21:57 -0500

    added time module to node 1 and reorganized file structure

--- node1/app.js
+++ node1/app.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     Firebase = require("firebase"),
     ref = new Firebase("https://securepenning.firebaseio.com/"),
+    timeModule = require('../time'),
     GPIO = require('./gpio');
     // Uncomment the line below if you want to test/ debug on a non-raspberry pi
     // GPIO = require('./consoleGPIO');
@@ -11,7 +12,7 @@ var express = require('express'),
 ref.on("value", function(snapshot){
   var data = snapshot.val();
   console.log("fb data", data);
-  console.log("time of fb change", new Date().toLocaleTimeString());
+  console.log("time of fb change", timeModule.localTime());
 
   // Arm the system
   if(data.arm.alarmSystem === "Armed" && data.arm.armedWithMotion){
@@ -24,7 +25,7 @@ ref.on("value", function(snapshot){
 
   // Disarm the system
   if(data.arm.alarmSystem !== "Armed") {
-    console.log("disarmed at: ", new Date().toLocaleTimeString());
+    console.log("disarmed at: ", timeModule.localTime());
     GPIO.disarm();
   }
 

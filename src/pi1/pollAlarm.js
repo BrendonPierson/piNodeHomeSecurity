e720ed0830fe0f459bf890047f8c876bdc07b13a
Brendon Pierson <BrendonPierson@gmail.com>
2016-02-07 13:56:30 -0600

    Added forever

--- src/pi1/pollAlarm.js
+++ src/pi1/pollAlarm.js
@@ -4,11 +4,15 @@ import Firebase from 'firebase'
 
 const ref = new Firebase("https://securepenning.firebaseio.com/")
 
+function soundSiren(data) {
+  setTimeout(() => ref.child('security').child('siren').set(1), data.enterDelay * 1000)
+}
+
 export default function pollAlarm(data) {
 
   if ((data.backDoor || data.frontDoor) && data.armed) {
-    setTimeout(() => ref.child('security').child('siren').set(1))
+    soundSiren(data)
   } else if (data.armedWithMotion && data.motion) {
-    setTimeout(() => ref.child('security').child('siren').set(1))
+    soundSiren(data)
   }
 }
